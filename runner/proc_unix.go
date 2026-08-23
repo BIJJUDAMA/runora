2c9670bd27e81b9879ae4d3f314b557097094e2d
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2026-08-23 06:23:03 +0530

    fix: add process group signal termination

---
+++ runner/proc_unix.go
@@ -0,0 +1,39 @@
+﻿//go:build !windows
+
+package runner
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func configureSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+}
+
+func interruptProcess(proc *os.Process) error {
+	if proc == nil {
+		return nil
+	}
+	// Send SIGINT to the process group
+	pgid, err := syscall.Getpgid(proc.Pid)
+	if err == nil {
+		return syscall.Kill(-pgid, syscall.SIGINT)
+	}
+	return proc.Signal(os.Interrupt)
+}
+
+func killProcess(proc *os.Process) error {
+	if proc == nil {
+		return nil
+	}
+	// Force kill the process group
+	pgid, err := syscall.Getpgid(proc.Pid)
+	if err == nil {
+		return syscall.Kill(-pgid, syscall.SIGKILL)
+	}
+	return proc.Kill()
+}
---
+++ runner/proc_windows.go
@@ -0,0 +1,40 @@
+﻿//go:build windows
+
+package runner
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func configureSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+	}
+}
+
+func interruptProcess(proc *os.Process) error {
+	if proc == nil {
+		return nil
+	}
+	// On Windows, send CTRL_BREAK_EVENT to the process group created with CREATE_NEW_PROCESS_GROUP.
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	procGenerateConsoleCtrlEvent := kernel32.NewProc("GenerateConsoleCtrlEvent")
+	if procGenerateConsoleCtrlEvent.Find() == nil {
+		r1, _, _ := procGenerateConsoleCtrlEvent.Call(syscall.CTRL_BREAK_EVENT, uintptr(proc.Pid))
+		if r1 != 0 {
+			return nil
+		}
+	}
+	// Fallback to os.Interrupt
+	_ = proc.Signal(os.Interrupt)
+	return nil
+}
+
+func killProcess(proc *os.Process) error {
+	if proc == nil {
+		return nil
+	}
+	return proc.Kill()
+}
